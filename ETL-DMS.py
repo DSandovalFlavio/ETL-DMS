eb10dc8b5733e2da33d6bc49c8b7966a5c84a0ca
dsandovalflavio <flaviocesarsandoval@gmail.com>
2023-11-04 11:27:55 -0600

    update

--- ETL-DMS.py
+++ ETL-DMS.py
@@ -25,3 +25,20 @@ from public.customers;
 '''
 df = pd.read_sql(query, engine)
 # %%
+path = 'data\data_dms_20180101.csv'
+df_raw_data = pd.read_csv(path)
+# %%
+query_ship_modes = '''
+select *
+from public.ship_modes
+'''
+df_ship_modes = pd.read_sql(query_ship_modes, engine)
+# %%
+df_unido = pd.merge(
+    df_raw_data[['Order_ID', 'Ship_Mode']],
+    df_ship_modes,
+    left_on='Ship_Mode',
+    right_on='shipmodeName',
+    how='left'
+)
+# %%
